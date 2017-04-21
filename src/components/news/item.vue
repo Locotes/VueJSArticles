25793df9f906f47bb41af3136bc5c8989b84a30c
Marc Veens <MarcVe@macaw.nl>
2017-04-21 16:19:05 +0200

    Added moment in order to format dates

--- src/components/news/item.vue
+++ src/components/news/item.vue
@@ -9,6 +9,7 @@
             <h4 class="media-heading">
                 <router-link :to="`/article/${item.id}`">{{item.title}}</router-link>
             </h4>
+            <time class="time">{{item.pubDate | format('YYYY-MM-DD')}}</time>
             <p>
                 {{item.description}} <router-link :to="`/article/${item.id}`">Read more &rarr;</router-link>
             </p>
@@ -17,8 +18,35 @@
 </template>
 
 <script>
+    import moment from 'moment';
     export default {
         name: 'newsItem',
-        props: ['item']
+        props: ['item'],
+        methods: {
+            moment: () => moment()
+        },
+        filters: {
+            format: (value, options) => moment(value).format(options)
+        }
     }
 </script>
+
+<style lang="scss">
+.news-item {
+    padding: 10px;
+    background-color: #EEE;
+    transition: opacity .5s ease;
+
+    .time {
+        display: block;
+        opacity: .7;
+        font-size: 12px;
+        margin-bottom: 5px;
+    }
+
+    .media-body {
+        vertical-align: middle;
+    }
+}
+
+</style>
--- src/components/news/list.vue
+++ src/components/news/list.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="container">
         <div class="news-items">
-            <transition-group name="fade-in" tag="div">
-                <news-item v-for="newsItem in newsItems" v-bind:key="newsItem.id" v-bind:item="newsItem"></news-item>
-            </transition-group>
+            <news-item v-for="newsItem in newsItems" v-bind:key="newsItem.id" v-bind:item="newsItem"></news-item>
             
             <div class="text-center">
                 <br />
-                <button class="btn btn-lg btn-primary" v-on:click="loadMoreItems()" v-show="loadMoreLink">Load more</button>
+                <nav>
+                    <ul class="pagination">
+                        <li v-show="page.prev">
+                            <span v-on:click="getPage(page.prev)" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </span>
+                        </li>
+                        <li v-bind:class="{active: page.current === thisPage.number}" v-for="thisPage in page.list"><span v-on:click="getPage(thisPage)">{{thisPage.number}}</span></li>
+                        <li v-show="page.next">
+                            <span  v-on:click="getPage(page.next)" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
@@ -27,20 +39,56 @@
         data: () => {
             return {
                 newsItems: [],
-                loadMoreLink: null
+                page: {
+                    list: [],
+                    prev: null,
+                    next: null,
+                    total: 1,
+                    current: 1,
+                    urlTemplate: ''
+                }
             }
         },
         created() { 
             this.loadMoreItems();
         },
         methods: {
-            loadMoreItems() { 
-                apiService.getArticlesByPage(this.loadMoreLink).then(({data, headers}) => {
+            loadMoreItems(url) { 
+                apiService.getArticlesByPage(url).then(({data, headers}) => {
                     let link = apiService.parseLinkHeader(headers.link);
 
-                    this.newsItems = this.newsItems.concat(data);
-                    this.loadMoreLink = link.next || null;
+                    this.page.urlTemplate = link.first.replace(/page=\d+/g, 'page={0}');
+                    this.newsItems = data;
+
+                    this.generatePagination(link);
                 });
+            },
+            generatePagination(link) {
+                this.page.total =  this.getPageNumber(link.last);
+                this.page.prev = link.prev ? { 
+                    number: this.getPageNumber(link.prev),
+                    url: link.prev
+                 } : null;
+                this.page.next = link.next ? { 
+                    number: this.getPageNumber(link.next),
+                    url: link.next
+                 } : null;  
+
+                if (!this.page.list.length){ 
+                    for (let i = 1; i <= this.page.total; i++) {
+                        this.page.list.push({
+                            number: i,
+                            url: this.page.urlTemplate.replace('{0}', i)
+                        });
+                    }
+                }
+            },
+            getPage(page) {
+                this.loadMoreItems(page.url);
+                this.page.current = page.number;
+            },
+            getPageNumber(link) {
+                return Number(link.match(/page=\d+/g)[0].replace('page=', ''))
             }
         }
     }
@@ -51,13 +99,5 @@
     padding: 10px 0;
 }
 
-.news-item {
-    padding: 10px;
-    background-color: #EEE;
-    transition: opacity .5s ease;
-
-    .media-body {
-        vertical-align: middle;
-    }
-}
+.pagination span {cursor:pointer;}
 </style>
